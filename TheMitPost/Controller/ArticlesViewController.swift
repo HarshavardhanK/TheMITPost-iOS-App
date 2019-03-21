fa94cf561be70a011103315c5c70880afa3c8b2f
Harshavardhan K <harshavardhan816@gmail.com>
2019-03-22 01:44:32 +0530

    Added collectionView cell diagonal animation

--- TheMitPost/Controller/ArticlesViewController.swift
+++ TheMitPost/Controller/ArticlesViewController.swift
@@ -243,6 +243,22 @@ class ArticlesViewController: UIViewController, UICollectionViewDelegate, UIColl
         return 0.0
     }
     
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        let transform = CATransform3DTranslate(CATransform3DIdentity, -200, -100, -10)
+        cell.layer.transform = transform
+        cell.alpha = 0.3
+        
+        UIView.animate(withDuration: 0.9, delay: 0.1, options: .curveEaseOut, animations: {
+            
+           // transform = CATransform3DTranslate(CATransform3DIdentity, 500, 10, 2)
+            cell.layer.transform = CATransform3DIdentity
+            cell.alpha = 1.0
+            
+        }) { (true) in
+            print("Animation complete")
+        }
+    }
     
     //MARK:- UICOLLECTIONVIEWDATASOURCE DELEGATE METHODS
     
